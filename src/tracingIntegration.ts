d9f06fc973d5f5e3adc5f279ba8332827e39d1c8
Nikody Keating <nkeating@mutualofenumclaw.com>
2020-12-17 14:06:04 -0800

    Adding a tracing custom resource

---
+++ src/tracingIntegration.ts
@@ -0,0 +1,31 @@
+import { send } from 'cfn-response';
+import { SSM } from 'aws-sdk';
+
+const ssm = new SSM();
+
+export async function handler(event, context) {
+    console.log(JSON.stringify(event));
+    const data = {
+        EpsagonToken: '',
+        EpsagonAppName: ''
+    };
+
+    try {
+        const params = await ssm.getParameters({
+            Names: [
+                process.env.epsagonTokenPath, 
+                process.env.epsagonAppNamePath
+            ]
+        }).promise();
+
+        const epsagonAppName = params.Parameters.find(x => x.Name === process.env.epsagonAppNamePath);
+        const epsagonToken = params.Parameters.find(x => x.Name === process.env.epsagonAppNamePath);
+
+        data.EpsagonAppName = epsagonAppName?.Value || '';
+        data.EpsagonToken = epsagonToken?.Value || '';
+    } catch (err) {
+        console.log(err);
+    }
+
+    send(event, context, 'SUCCESS', data);
+}
